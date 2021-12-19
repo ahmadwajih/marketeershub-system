192ae25b70fab3a033afd9e35b497294327ab07d
Abdul Shakoor <abdulshaqoor@gmail.com>
2021-12-19 14:55:18 +0300

    modal for offer request via ajax

--- resources/views/admin/offers/index_new.blade.php
+++ resources/views/admin/offers/index_new.blade.php
@@ -25,9 +25,7 @@
         table.dataTable thead tr,
         table.dataTable tbody tr {
             z-index: 1;
-        }
-        .offer-request-modal .modal-dialog {
-            max-width: 960px;
+
         }
     </style>
 @endpush
@@ -77,7 +75,8 @@
                                     </td>
                                     <td>{{ $offer->name }}</td>
                                     <td>{{ __('Online') }}</td>
-                                    <td>@if($offer->cps_type == 'static')
+                                    <td>                     @if($offer->cps_type == 'static')
+
                                             {{$offer->revenu}}
                                         @elseif($offer->cps_type == 'new_old')
                                             new_old
@@ -114,13 +113,77 @@
                                             @if($offer->type == 'link_tracking')
                                                 <button class="rounded requestOffer btn btn-warning btn-xs mr-2" data-modal="{{ 'modal'.$offer->id }}">{{ __('Request Link') }}</button>
                                             @elseif($offer->type == 'coupon_tracking')
-                                                <button class="rounded requestOffer btn btn-warning btn-xs mr-2 request-coupons" data-offer="{{ $offer->id }}">{{ __('Request Coupons') }}</button>
+                                                <button class="rounded requestOffer btn btn-warning btn-xs mr-2" data-modal="{{ 'modal'.$offer->id }}">{{ __('Request Coupons') }}</button>
                                             @endif
                                         @endif
                                         @if($update)
                                             <a class="edit-offer btn btn-icon btn-success btn-xs mr-2" href="{{ route('admin.offers.edit', $offer->id) }}"><i class="fas fa-pen"></i></a>
                                         @endif
                                         <a class="show-offer btn btn-icon btn-success btn-xs" href="{{ route('admin.offers.show', $offer->id) }}"><i class="fas fa-eye"></i></a>
+                                        <div class="modal" tabindex="-1" role="dialog" id='modal{{ $offer->id }}'>
+                                            <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title">{{ __('To Continue you must agree to the T&Cs') }}</h5>
+                                                        <button type="button" class="close close-modal" data-dismiss="modal" aria-label="Close">
+                                                            <i class="far fa-times-circle"></i>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <div class="english" dir="ltr">
+                                                            <h2>Restrictions</h2>
+                                                            <ul>
+                                                                <li>You may not bid on any of the
+                                                                    <b>{{ $offer->name }}</b> terms or variations in paid search ads, such as Google Adwords, Google PPC, and FaceBook Ads.
+                                                                </li>
+                                                                <li>You may not use the
+                                                                    <b>{{ $offer->name }}</b> name or any of its variations in pop-ups and pop-unders, as the name of your newsletter, in retargeting campaigns, in your app push notifications ads, or in wrong or misleading messages.
+                                                                </li>
+                                                                <li>You may not use methods such as cookie stuffing.</li>
+                                                                <li>ou may not promote
+                                                                    <b>{{ $offer->name }}</b> in any sexually explicit materials, violent materials, libelous or defamatory materials, or any illegal activities.
+                                                                </li>
+                                                                <li>You may not promote
+                                                                    <b>{{ $offer->name }}</b> if you employ discriminatory practices, based on race, sex, religion, nationality, disability, sexual orientation, or age.
+                                                                </li>
+                                                                <li>You may not use a link to
+                                                                    <b>{{ $offer->name }}</b> which includes a redirecting link, that is generated or displayed on a Search Engine in response to a general Internet keyword search query, whether those links appear through your submission of data to that site or otherwise.
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                        <div class="arabic text-right" dir="rtl">
+                                                            <h2>تقييدات</h2>
+                                                            <ul>
+                                                                <li>لا يحق لك المُزايدة على أي من عبارات ومُصطلحات
+                                                                    <b>{{ $offer->name }}</b> المدفوعة مُسبقًا على شبكة البحث, مثل- Google Adwords, Google PPC و إعلانات فيسبوك.
+                                                                </li>
+                                                                <li>لا يجوز لك استخدام اسم
+                                                                    <b>{{ $offer->name }}</b> أو أي من أشكاله في نوافذ الإعلانات المُنبثقة في الأعلى وفي الخلف, مثل اسم نشرتك الإعلانية لإعادة توجيه الحملات, في إعلانات تطبيق إشعارات الدّفع, أو من خلال رسائل خاطئة ومُضلّلة.
+                                                                </li>
+                                                                <li>لا يجوز لك استخدام أساليب كحشو الْــ cookie.</li>
+                                                                <li>للا يجوز لك التّرويج لِـــ
+                                                                    <b>{{ $offer->name }}</b> في أي مواد جنسية واضحة أو مواد عنيفة أو مواد تشهيريّة أو أي افتراء أو نشاط غير قانوني.
+                                                                </li>
+                                                                <li>لا يجوز لك التّرويج لِـــ
+                                                                    <b>{{ $offer->name }}</b> إذا كنت تستخدم عادات تمييزية, على أساس العرق, الجنس, الدين, القومية, الإعاقة الجسدية, التوجّه الجنسي أو العمر.
+                                                                </li>
+                                                                <li>للا يجوز لك استخدام رابط لِــ
+                                                                    <b>{{ $offer->name }}</b> يحتوي على رابط إعادة توجيه, يتم إنشاؤه أو عرضه على مُحرّك البحث استجابة لطلب بحث الكلمة الرئيسيّة على الانترنت, سواء كانت هذه الروابط تظهر خلال إرسال المعلومات إلى الموقع أو غير ذلك.
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer text-center">
+                                                        @if($offer->type == 'link_tracking')
+                                                            <button type="button" class="btn btn-primary request-codes" data-offer="{{ $offer->id }}">{{ __('Request Link') }}</button>
+                                                        @elseif($offer->type == 'coupon_tracking')
+                                                            <button type="button" class="btn btn-primary request-codes" data-offer="{{ $offer->id }}">{{ __('Request Codes') }}</button>
+                                                        @endif
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </td>
                                 </tr>
                             @endforeach
@@ -138,70 +201,6 @@
         </div>
         <!--end::Container-->
     </div>
-  {{--  <div class="modal" tabindex="-1" role="dialog" id='modal{{ $offer->id }}'>
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">{{ __('To Continue you must agree to the T&Cs') }}</h5>
-                    <button type="button" class="close close-modal" data-dismiss="modal" aria-label="Close">
-                        <i class="far fa-times-circle"></i>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="english" dir="ltr">
-                        <h2>Restrictions</h2>
-                        <ul>
-                            <li>You may not bid on any of the
-                                <b>{{ $offer->name }}</b> terms or variations in paid search ads, such as Google Adwords, Google PPC, and FaceBook Ads.
-                            </li>
-                            <li>You may not use the
-                                <b>{{ $offer->name }}</b> name or any of its variations in pop-ups and pop-unders, as the name of your newsletter, in retargeting campaigns, in your app push notifications ads, or in wrong or misleading messages.
-                            </li>
-                            <li>You may not use methods such as cookie stuffing.</li>
-                            <li>ou may not promote
-                                <b>{{ $offer->name }}</b> in any sexually explicit materials, violent materials, libelous or defamatory materials, or any illegal activities.
-                            </li>
-                            <li>You may not promote
-                                <b>{{ $offer->name }}</b> if you employ discriminatory practices, based on race, sex, religion, nationality, disability, sexual orientation, or age.
-                            </li>
-                            <li>You may not use a link to
-                                <b>{{ $offer->name }}</b> which includes a redirecting link, that is generated or displayed on a Search Engine in response to a general Internet keyword search query, whether those links appear through your submission of data to that site or otherwise.
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="arabic text-right" dir="rtl">
-                        <h2>تقييدات</h2>
-                        <ul>
-                            <li>لا يحق لك المُزايدة على أي من عبارات ومُصطلحات
-                                <b>{{ $offer->name }}</b> المدفوعة مُسبقًا على شبكة البحث, مثل- Google Adwords, Google PPC و إعلانات فيسبوك.
-                            </li>
-                            <li>لا يجوز لك استخدام اسم
-                                <b>{{ $offer->name }}</b> أو أي من أشكاله في نوافذ الإعلانات المُنبثقة في الأعلى وفي الخلف, مثل اسم نشرتك الإعلانية لإعادة توجيه الحملات, في إعلانات تطبيق إشعارات الدّفع, أو من خلال رسائل خاطئة ومُضلّلة.
-                            </li>
-                            <li>لا يجوز لك استخدام أساليب كحشو الْــ cookie.</li>
-                            <li>للا يجوز لك التّرويج لِـــ
-                                <b>{{ $offer->name }}</b> في أي مواد جنسية واضحة أو مواد عنيفة أو مواد تشهيريّة أو أي افتراء أو نشاط غير قانوني.
-                            </li>
-                            <li>لا يجوز لك التّرويج لِـــ
-                                <b>{{ $offer->name }}</b> إذا كنت تستخدم عادات تمييزية, على أساس العرق, الجنس, الدين, القومية, الإعاقة الجسدية, التوجّه الجنسي أو العمر.
-                            </li>
-                            <li>للا يجوز لك استخدام رابط لِــ
-                                <b>{{ $offer->name }}</b> يحتوي على رابط إعادة توجيه, يتم إنشاؤه أو عرضه على مُحرّك البحث استجابة لطلب بحث الكلمة الرئيسيّة على الانترنت, سواء كانت هذه الروابط تظهر خلال إرسال المعلومات إلى الموقع أو غير ذلك.
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="modal-footer text-center">
-                    @if($offer->type == 'link_tracking')
-                        <button type="button" class="btn btn-primary request-codes" data-offer="{{ $offer->id }}">{{ __('Request Link') }}</button>
-                    @elseif($offer->type == 'coupon_tracking')
-                        <button type="button" class="btn btn-primary request-codes" data-offer="{{ $offer->id }}">{{ __('Request Codes') }}</button>
-                    @endif
-
-                </div>
-            </div>
-        </div>
-    </div>--}}
     <!--end::Entry-->
 @endsection
 @push('scripts')
@@ -215,8 +214,8 @@
             });
 
             // Close Modal
-            $("body").on('click', '.close-modal', function ()  {
-                $('.modal').fadeOut('slow');
+            $(".close-modal").click(function () {
+                $('.modal').css('display', 'none');
             });
 
             // Send Request
@@ -248,6 +247,8 @@
                         setTimeout(function () {
                             $('.modal').fadeOut('slow');
                         }, 3000);
+
+
                     });
             });
 
@@ -268,29 +269,6 @@
                     })
                     .fail(function (response) {
                     });
-            });
-
-            // Send CouponsRequest
-            $(".request-coupons").click(function () {
-                var $self = $(this);
-                var offerId = $self.data('offer');
-                $.ajax({
-                    method: "GET",
-                    cache: false,
-                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                    url: "{{route('admin.offerRequest.ajax.form')}}",
-                    data: {
-                        offer_id: offerId,
-                    },
-                })
-                    .done(function (res) {
-                        $(".requestOffer").removeClass('requestOffer');
-                        $(res).insertAfter('#kt_footer')
-                            .addClass('offer-request-modal').fadeIn('slow')
-                            .end()
-                            .next('.modal')
-                            .remove();
-                    })
             });
 
 
