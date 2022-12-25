0b7596705c881129e456404a167be6a2ed85a8df
naderm1991 <nadermohammad9@gmail.com>
2022-12-25 13:41:00 +0200

    Bulk import -use laravel queue

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,6 +13,8 @@ class Controller extends BaseController
 
     protected function execute_command(string $command)
     {
-        shell_exec("/usr/local/bin/ea-php80 /home/systemmh/public_html/artisan $command > /dev/null &");
+        shell_exec(
+            "/usr/local/bin/ea-php80 /home/systemmh/public_html/artisan $command > /dev/null &"
+        );
     }
 }
