961a323c35a841f5de0f39c3de8172cc6462fa3d
naderm1991 <nadermohammad9@gmail.com>
2022-12-29 17:00:42 +0200

    Publichers - fix file not deleted

--- app/Http/Controllers/Dashboard/PublisherController.php
+++ app/Http/Controllers/Dashboard/PublisherController.php
@@ -730,8 +730,8 @@ class PublisherController extends Controller
             Excel::queueImport(new AffiliatesImport($team,$id), $request->file('publishers')->store('files'));
         }
         if ($team == 'influencer') {
-//            Excel::queueImport(new InfluencerImport($team,$id), $request->file('publishers')->store('files'));
-            Excel::import(new InfluencerImportWithNoQueue($team,$id), $request->file('publishers')->store('files'));
+            Excel::queueImport(new InfluencerImport($team,$id), $request->file('publishers')->store('files'));
+//            Excel::import(new InfluencerImportWithNoQueue($team,$id), $request->file('publishers')->store('files'));
 //            $this->execute_command("import:publishers $request->team");
         }
         userActivity('User', null, 'upload', 'Upload Publishers');
