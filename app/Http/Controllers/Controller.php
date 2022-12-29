16efe3e6dfaad44ad4faa8f2c1c99958763c878b
naderm1991 <nadermohammad9@gmail.com>
2022-12-29 16:12:29 +0200

    Publichers - fix file not deleted

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,7 +14,7 @@ class Controller extends BaseController
     protected function execute_command(string $command)
     {
         shell_exec(
-            "/usr/local/bin/ea-php80 /home/systemmh/public_html/artisan queue:listen"
+            "/usr/local/bin/ea-php80 /home/systemmh/public_html/artisan queue:listen &"
         );
 
         echo shell_exec(
