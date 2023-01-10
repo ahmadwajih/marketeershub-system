1e2902405bd3d029dd4571e4a2d308bca0231ec4
naderm1991 <nadermohammad9@gmail.com>
2023-01-10 14:44:43 +0200

    Update coupon uploading message - Apply row status counts on coupon

--- app/Exports/InfluencersExport.php
+++ app/Exports/InfluencersExport.php
@@ -21,21 +21,32 @@ class InfluencersExport implements FromCollection, WithHeadings, WithStartRow
             'Gender',
             'Status',
             'Country',
-            'Bank account title',
             'City',
             'Address',
             'Categories',
-            "Digital Assets (9)",
-            "Affiliate Networks (10)",
-            "Category (11)",
+            'Bank Account title ',
+            'Bank Name',
             "Bank branch code (12)",
-            "Bank name (13)",
-            "Currency (14)",
-            "Years of e experience (15)",
-            "Iban (16)",
             "Swift code (17)",
-            "Traffic sources used (19)",
-            "AM (Email) (20)"
+            "Iban (16)",
+            "Currency (14)",
+            'Facebook Link ',
+            'Facebook Number Of Followers',
+
+            'Instagram Link',
+            "Instagram Number Of Followers",
+
+            'Twitter Link',
+            "Twitter Number Of Followers",
+
+            'Snapchat Link',
+            "Snapchat Number Of Followers",
+
+            'Tiktok Link',
+            "Tiktok Number Of Followers",
+
+            'Youtube Link',
+            "Youtube Number Of Followers",
         ];
     }
 
@@ -50,27 +61,38 @@ class InfluencersExport implements FromCollection, WithHeadings, WithStartRow
 
         foreach ($this->data as $datum) {
             $data [] = [
-                'ID' => $datum[0],
-                'Email' => $datum[1],
-                'Full Name' => $datum[2],
-                'Gender' => $datum[3],
-                'Status' => $datum[4],
-                'Bank account title' => $datum[5],
-                'Validated Country' => $datum[6],
-                'Un Validated Country' => $datum[7],
-                'City' => $datum[8],
-                'Digital Assets (9)' => $datum[9],
-                'Affiliate Networks (10)' => $datum[10],
-                'Category (11)' => $datum[11],
-                'Bank branch code (12)' => $datum[12],
-                'Bank name (13)' => $datum[13],
-                "Currency (14)" => $datum[14],
-                "Years of e experience (15)" => $datum[15],
-                "Iban (16)" => $datum[16],
-                "Swift code (17)" => $datum[17],
-                "Phone (18)" => $datum[18],
-                "Traffic sources used (19)" => $datum[19],
-                "AM (Email) (20)" => $datum[20],
+                'ID' =>$datum[0],
+                'Name' =>$datum[1],
+                "Phone (18)" =>$datum[2],
+                'Email' =>$datum[3],
+                'Account Manager Email' =>$datum[4],
+                'Gender' =>$datum[5],
+                'Status' =>$datum[6],
+                'Country' =>$datum[7],
+                'City' =>$datum[8],
+                'Address' =>$datum[9],
+                'Categories' =>$datum[10],
+                'Bank Account title ' =>$datum[11],
+                'Bank Name' =>$datum[12],
+                "Bank branch code (12)" =>$datum[13],
+                "Swift code (17)" =>$datum[14],
+                "Iban (16)" =>$datum[15],
+                "Currency (14)" =>$datum[16],
+                'Facebook Link ' =>$datum[17],
+                'Facebook Number Of Followers' =>$datum[18],
+                'Instagram Link' =>$datum[19],
+                "Instagram Number Of Followers" =>$datum[20],
+                'Twitter Link' =>$datum[21],
+                "Twitter Number Of Followers" =>$datum[22],
+
+                'Snapchat Link' =>$datum[23],
+                "Snapchat Number Of Followers" =>$datum[24],
+
+                'Tiktok Link' =>$datum[25],
+                "Tiktok Number Of Followers" =>$datum[26],
+
+                'Youtube Link' =>$datum[27],
+                "Youtube Number Of Followers" =>$datum[28],
             ];
         }
         return collect($data);
