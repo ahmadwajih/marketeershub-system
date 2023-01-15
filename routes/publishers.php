d9b60ce4b695c73f7a8724cdd40dc533cf4f44a5
naderm1991 <nadermohammad9@gmail.com>
2023-01-15 16:31:04 +0200

    Publisher dashboard - add coupons

--- routes/publishers.php
+++ routes/publishers.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'publisher', 'namespace' => 'publisher', 'as' => 'publisher.'], function(){
+Route::group(['prefix' => 'publisher', 'namespace' => 'Publisher', 'as' => 'publisher.'], function(){
 
     Route::middleware(['auth:web'])->group(function (){
         Route::get('/', 'UserController@profile')->name('user.profile');
